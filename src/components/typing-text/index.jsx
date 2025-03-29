c6ae85512a83e9ac8d6ae9a864d4423f6c63da10
ruck <ruckshanth@speedcodelabs.com>
2025-03-29 22:11:05 +0530

    remove comment

--- src/components/typing-text/index.jsx
+++ src/components/typing-text/index.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 const Typewriter = ({ texts, delay }) => {
   const [currentText, setCurrentText] = useState('');
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [textIndex, setTextIndex] = useState(0); // New state to keep track of which text we're typing
+  const [textIndex, setTextIndex] = useState(0);
 
   useEffect(() => {
     if (textIndex < texts.length) {
@@ -17,12 +17,11 @@ const Typewriter = ({ texts, delay }) => {
 
         return () => clearTimeout(timeout);
       } else {
-        // When one text is finished, move on to the next text after a short delay
         const nextTextTimeout = setTimeout(() => {
           setCurrentText('');
           setCurrentIndex(0);
-          setTextIndex((prevIndex) => (prevIndex + 1) % texts.length); // Loop back to the first text if needed
-        }, 1000); // 1-second delay between texts (you can adjust this)
+          setTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
+        }, 1000);
 
         return () => clearTimeout(nextTextTimeout);
       }
